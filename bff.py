0a91ebc5f6e48d8d0e433cfdc7dbbd5209333689
Tenzin Dorjee <tenzindorjee@outlook.com>
2020-05-06 15:05:22 -0700

    Update bff.py
    updated stuff

--- bff.py
+++ bff.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 
 @app.route('/<string:community>/<int:amount>')
 def ParticularRecent(amount,community):
-	# newRoute = '127.0.0.1:5000/api/v1.0/resources/collections/recent?community=' + community + '&amount=' +str(amount)
-	newRoute = 'http://127.0.0.1:5000/api/v1.0/resources/collections/recent?community=CSUF&amount=15'
+	newRoute = 'http://127.0.0.1:5000/api/v1.0/resources/collections/recent?community=' + community + '&amount=' +str(amount)
 	
 	response = requests.get(newRoute)
 	jsonDict = response.json()
@@ -19,16 +18,37 @@ def ParticularRecent(amount,community):
 	fg.language('en')
 	fg.description('printing the top 25 recent post for a particular community!')
 
-	counter = 0
 	for post in jsonDict:
+	
 		fe = fg.add_entry()
 		fe.title(post['title'])
-		# fe.author(post['username'])
-		# fe.description(post['text'])
-		counter += 1
+		fe.author( {'name':post['username'],'email':'john@example.de'})
+		fe.id(post['community'])
+		
+	results = fg.rss_str(pretty=True)
+	return results
 
+@app.route('/any/<int:amount>')
+def any(amount):
+	newRoute = 'http://127.0.0.1:5000/api/v1.0/resources/collections/any?amount='+ str(amount)
+	response1 = requests.get(newRoute)
+	jsonDict1 = response1.json()
+
+	fg = FeedGenerator()
+	fg.id('http://127.0.0.1:5100/any/<int:amount>')
+	fg.link(href='http://127.0.0.1:5100/any/<int:amount>', rel='alternate')
+	fg.title('25 recent post for any community')
+	fg.language('en')
+	fg.description('printing the top 25 recent post for any community!')
+
+	for post in jsonDict1:
+		fe = fg.add_entry()
+		fe.title(post['title'])
+		fe.author( {'name':post['username'],'email':'john@example.de'})
+		fe.id(post['community'])
 	
 	results = fg.rss_str(pretty=True)
-	
 	return results
+
+
 	
